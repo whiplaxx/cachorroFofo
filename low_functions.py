bde28f6abe9c51fbed23234d292ff1d2bce29502
whiplaxx <whiplaxx@gmail.com>
2020-04-27 14:33:53 -0300

    mudancas

--- low_functions.py
+++ low_functions.py
@@ -1,7 +1,15 @@
-def startsWith(string1, string2):
+def startsWith(string1, string2, ignoreSpaces = 1):
 
+    if ignoreSpaces == 1:
+        b = 0
+        while string1[b] == ' ' or string1[b] == '\n':
+            b += 1
+        
     for i in range(0, len(string2)):
-        if string1[i] != string2[i]:
+        if string1[i+b] != string2[i]:
             return False
 
     return True
+
+if __name__ == "__main__":
+    pass
